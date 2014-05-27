847350f91f5a8bb2ae04ddaa0bb145c01368c6d2
Jordan Catron <jordancatron@Jordans-MacBook-Air.local>
2014-05-26 22:09:48 -0400

    added a test class

---
+++ JECTest.h
@@ -0,0 +1,13 @@
+//
+//  JECTest.h
+//  GIT Test
+//
+//  Created by Jordan Catron on 5/26/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JECTest : NSObject
+
+@end
---
+++ JECTest.m
@@ -0,0 +1,13 @@
+//
+//  JECTest.m
+//  GIT Test
+//
+//  Created by Jordan Catron on 5/26/14.
+//
+//
+
+#import "JECTest.h"
+
+@implementation JECTest
+
+@end
